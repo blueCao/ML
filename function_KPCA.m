df5cfe87f763f7e0a03513345f2eb63d94a51e2c
caojunhui <770541944@qq.com>
2018-03-07 10:15:13 +0800

    KPCA（kernal PCA）经过核函数扩展后的PCA

--- function_KPCA.m
+++ function_KPCA.m
@@ -4,9 +4,37 @@ KPCA:kernal PCA
 input:
     X : each column is a data(dimension is n)
     k : the target dimension 
+
+output:
+    Y: the new dimension values
 %}
 function [Y] = function_KPCA(X,k)
-    %caculate the kernal
+    N = size(X,2);
+    n = size(X,1);
+    if k > N
+        sprintf('%s','error k must less than vector x‘s dimension!')
+        exit
+    end
     
+    %caculate the kernal
+    K = zeros(N,N);
+    for i = 1 :N
+       for j = 1 : N
+          K(i,j) = X(:,i)' * X(:,j); 
+       end
+    end
+    
+    %caculate K's eigent values and vectors,keep k largest eigent values
+    [V,lambda] = eig(K);
+    [lambda,sort_index]=sort(diag(lambda),'descend');
+    V=V(:,sort_index);
+    V=V(:,1:k);
+    
+    %caculate the transformed value
+    Y = zeros(k,N);
+    for i = 1 : N
+        for j = 1 : k
+            Y(j,i) = V(:,j)' * K(:,i) / sqrt(lambda(j,1));
+        end
+    end
 end
-
