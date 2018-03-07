49482dc33904b3f0e01a8a8562bcae092d386656
Ove <770541944@qq.com>
2018-03-07 08:32:21 +0800

    待完成！KPCA

---
+++ function_KPCA.m
@@ -0,0 +1,12 @@
+%{
+KPCA:kernal PCA
+
+input:
+    X : each column is a data(dimension is n)
+    k : the target dimension 
+%}
+function [Y] = function_KPCA(X,k)
+    %caculate the kernal
+    
+end
+
