031adef658bcc9d427c92be5d90a18f56057adb2
Ove <770541944@qq.com>
2018-03-09 13:27:39 +0800

    Kmeans均值聚类

--- function_kmeans.m
+++ function_kmeans.m
@@ -14,26 +14,26 @@ function [center,matrix_0_1] = function_kmeans(X,k)
     m = size(X,1);  %data dimension
     N = size(X,2);  %data size
     %1.randomly selects two data as center points
-    center = zerso(m,k);
-    rand_index = ceil(rand(1,N) * N);
+    center = zeros(m,k);
+    rand_index = ceil(rand(1,k) * N);
     for i = 1 : k
         center(:,i) = X(:,rand_index(1,i));
     end
+    center_before = center;
     %2. iterator: find the center point
-    center_1 = zerso(m,k);
     matrix_0_1 = zeros(k,N);
     while 1
         distances = zeros(1,k);
         matrix_0_1(:) = 0;
-        center = zerso(m,k);
         for i = 1 : N
             for j = 1 : k
-               distances(j) = sqrt((X(:,i) - center(:,j)' * (X(:,i) - center(:,j)); % distance between center j and data i
+               distances(j) = (X(:,i) - center(:,j))' * (X(:,i) - center(:,j)); % distance between center j and data i
             end
-            [v_min,index] = min(distance);
+            [v_min,index] = min(distances);
             matrix_0_1(index,i) = 1;    % data i belongs to closest center
         end
         % caculate the new centers
+        center = zeros(m,k);
         for i = 1 : k
            for j = 1 : N
                if matrix_0_1(i,j) == 1
@@ -41,7 +41,11 @@ function [center,matrix_0_1] = function_kmeans(X,k)
                end
            end
            center(:,i) = center(:,i) / sum( matrix_0_1(i,:)); % caculate the average center
-           
         end
+        % exit when convergence
+        if center == center_before
+            break;
+        end
+        center_before = center;
     end
 end
