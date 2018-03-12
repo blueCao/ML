3d6f06d87c974fb189b0edbdc0a8f51bc0a480cb
caojunhui <770541944@qq.com>
2018-03-12 14:45:12 +0800

    多维高斯分布概率概率密度计算公式，支持多个数据（多列）一次性计算

--- function_gauss_pdf.m
+++ function_gauss_pdf.m
@@ -2,7 +2,7 @@
 guassian probability distribute function
 
 input:
-    x:      mx1 column vector
+    x:      mxn each colum is a column vector,has n value
     mu:     mx1 mean value,column vector
     sigma:  mxm coefficient matrix
 
@@ -12,13 +12,15 @@ output:
 function [ pdf_value ] = function_gauss_pdf(x, mu, sigma)
     if size(x,1) ~= size(mu,1) ||   size(x,1) ~= size(sigma,1) || size(sigma,1) ~= size(sigma,2)
        disp('x, mu, sigma must has the same dimension')
-       exit; 
     end
     m = size(x,1);
+    n = size(x,2);
     det_value = det(sigma);
     if det_value == 0
        disp('the sigma matrix det value cannot be zero')
-       exit;
     end
-    pdf_value = (2*pi)^(-m/2) * det_value^(-0.5) * exp( -0.5/pi * (x - mu)' * inv(sigma) * (x - mu));
+    pdf_value = zeros(n,1);
+    for i = 1 : n
+        pdf_value(i) = (2*pi)^(-m/2) * det_value^(-0.5) * exp( -0.5/pi * (x(:,i) - mu)' * inv(sigma) * (x(:,i) - mu));
+    end
 end
